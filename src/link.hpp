369689b6797bbf4b0e1de98095a7935c00ef32f8
Danilo Borquez-Paredes <danilo.borquez@gmail.com>
2020-10-24 11:32:27 -0300

    Adding comments to both classes created. Documentation is created

--- src/link.hpp
+++ src/link.hpp
@@ -1,13 +1,42 @@
 #ifndef __LINK_H__
 #define __LINK_H__
 
+/**
+ * @brief Class with the link information.
+ *
+ * This class represents to each link in the physical network.
+ *
+ */
+
 class Link {
  public:
+  /**
+   * @brief Construct a new Link object
+   *
+   */
   Link(void);
+  /**
+   * @brief Construct a new Link object
+   *
+   * @param id The id of this new Link object
+   */
   Link(int id);
+  /**
+   * @brief Destroy the Link object
+   *
+   */
   ~Link();
-
+  /**
+   * @brief Set the Id object
+   *
+   * @param id The new Id of this Link object
+   */
   void setId(int id);
+  /**
+   * @brief Get the Id object
+   *
+   * @return int The Id of this Link object
+   */
   int getId(void);
 
  private:
--- src/node.hpp
+++ src/node.hpp
@@ -3,17 +3,60 @@
 
 #include <string>
 
+/**
+ * @brief Class with the node information.
+ *
+ * This class represents to each node in the physical network.
+ */
+
 class Node {
  public:
+  /**
+   * @brief Construct a new Node object
+   *
+   */
   Node(void);
+  /**
+   * @brief Construct a new Node object
+   *
+   * @param id The id of the new Node
+   */
   Node(int id);
+  /**
+   * @brief Construct a new Node object
+   *
+   * @param id The id of the new Node
+   * @param label The label of the new Node
+   */
   Node(int id, std::string label);
+  /**
+   * @brief Destroy the Node object
+   *
+   */
   ~Node();
-
+  /**
+   * @brief Set the Id object
+   *
+   * @param id the new id of this Node object
+   */
   void setId(int id);
+  /**
+   * @brief Get the Id object
+   *
+   * @return int the Id of this Node object
+   */
   int getId(void);
-
+  /**
+   * @brief Set the Label object
+   *
+   * @param label The new label of this Node object
+   */
   void setLabel(std::string label);
+  /**
+   * @brief Get the Label object
+   *
+   * @return std::string The label of this Node object
+   */
   std::string getLabel(void);
 
  private:
