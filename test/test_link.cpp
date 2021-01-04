2ba6f27641b237a9aaa2b12ee80dcf50235d777d
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-01-04 12:01:27 -0300

    Test link fixed in ids of links

--- test/test_link.cpp
+++ test/test_link.cpp
@@ -9,54 +9,54 @@ TEST_CASE("#1 Constructor (Link)") {
   CHECK_NOTHROW(Link(0));
 }
 
-TEST_CASE("#2 Constructor (Link)") { CHECK_NOTHROW(Link(0, 70.0)); }
+TEST_CASE("#2 Constructor (Link)") { CHECK_NOTHROW(Link(1, 70.0)); }
 
-TEST_CASE("#3 Constructor (Link)") { CHECK_NOTHROW(Link(0, 70.0, 200)); }
+TEST_CASE("#3 Constructor (Link)") { CHECK_NOTHROW(Link(2, 70.0, 200)); }
 
 TEST_CASE("#4 Constructor (Link)") {
   CHECK_NOTHROW(Link());
-  CHECK_NOTHROW(Link(0));
-  CHECK_NOTHROW(Link(1, 70.0));
-  CHECK_NOTHROW(Link(2, 70.0, 200));
-  CHECK_NOTHROW(Link(3, 70.0, 200));
+  CHECK_NOTHROW(Link(3));
+  CHECK_NOTHROW(Link(4, 70.0));
+  CHECK_NOTHROW(Link(5, 70.0, 200));
+  CHECK_NOTHROW(Link(6, 70.0, 200));
 }
 
 TEST_CASE("Constructor Exception(Link)") {
-  CHECK_THROWS(Link(0, 0.0));
-  CHECK_THROWS(Link(0, 1.1, 0));
-  CHECK_THROWS(Link(0, -0.1, 11));
+  CHECK_THROWS(Link(7, 0.0));
+  CHECK_THROWS(Link(8, 1.1, 0));
+  CHECK_THROWS(Link(9, -0.1, 11));
 }
 
 TEST_CASE("Getting ID (Link)") {
   Link l1 = Link();
-  Link l2 = Link(0);
-  Link l3 = Link(1, 70.0);
-  Link l4 = Link(2, 70.0, 200);
+  Link l2 = Link(10);
+  Link l3 = Link(11, 70.0);
+  Link l4 = Link(12, 70.0, 200);
 
   CHECK(l1.getId() == -1);
-  CHECK(l2.getId() == 0);
-  CHECK(l3.getId() == 1);
-  CHECK(l4.getId() == 2);
+  CHECK(l2.getId() == 10);
+  CHECK(l3.getId() == 11);
+  CHECK(l4.getId() == 12);
 }
 
 TEST_CASE("Setting ID (Link)") {
   Link l1 = Link();
-  l1.setId(0);
-  CHECK(l1.getId() == 0);
+  l1.setId(13);
+  CHECK(l1.getId() == 13);
   Link l2 = Link();
-  l2.setId(1);
-  CHECK(l2.getId() == 1);
+  l2.setId(14);
+  CHECK(l2.getId() == 14);
 }
 
 TEST_CASE("Getting length (Link)") {
-  Link l1 = Link(0, 70.0, 200);
-  Link l2 = Link(1, 80.0, 200);
+  Link l1 = Link(15, 70.0, 200);
+  Link l2 = Link(16, 80.0, 200);
   CHECK(l1.getLength() == 70.0);
   CHECK(l2.getLength() == 80.0);
 }
 
 TEST_CASE("Setting length (Link)") {
-  Link l1 = Link(0, 70.0, 200);
+  Link l1 = Link(17, 70.0, 200);
 
   l1.setLength(10.0);
 
@@ -64,13 +64,13 @@ TEST_CASE("Setting length (Link)") {
 }
 
 TEST_CASE("Getting slots (Link)") {
-  Link l1 = Link(0, 70.0, 200);
+  Link l1 = Link(18, 70.0, 200);
 
   CHECK(l1.getSlots() == 200);
 }
 
 TEST_CASE("Setting slots (Link)") {
-  Link l1 = Link(0, 70.0, 200);
+  Link l1 = Link(19, 70.0, 200);
 
   l1.setSlots(100);
 
@@ -80,7 +80,7 @@ TEST_CASE("Setting slots (Link)") {
 }
 
 TEST_CASE("Setting slot (Link)") {
-  Link l1 = Link(0, 70.0, 200);
+  Link l1 = Link(20, 70.0, 200);
 
   l1.setSlots(100);
   l1.setSlot(50, true);
@@ -93,7 +93,7 @@ TEST_CASE("Setting slot (Link)") {
 }
 
 TEST_CASE("getting slot (Link)") {
-  Link l1 = Link(0, 70.0, 200);
+  Link l1 = Link(21, 70.0, 200);
 
   CHECK(l1.getSlot(50) == false);
   CHECK_THROWS(l1.getSlot(-1) == false);
