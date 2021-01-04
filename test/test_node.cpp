d3c501ee6d98ed42c3825766fbd559fd25758650
Alvaro Robert <alvaro.robser@gmail.com>
2021-01-04 11:20:16 -0300

    tests updated and fixed

--- test/test_node.cpp
+++ test/test_node.cpp
@@ -7,23 +7,24 @@
 TEST_CASE("Constructor (Node)") {
   CHECK_NOTHROW(Node());
   CHECK_NOTHROW(Node(0));
-  CHECK_NOTHROW(Node(0, "Test node 0"));
+  CHECK_NOTHROW(Node(1, "Test node 1"));
 }
 
 TEST_CASE("Getting ID (Node)") {
   Node n1 = Node();
-  Node n2 = Node(50);
+  Node n2 = Node(0);
 
   CHECK(n1.getId() == -1);
-  CHECK(n2.getId() == 50);
+  CHECK(n2.getId() == 0);
 }
 
 TEST_CASE("Setting ID (Node)") {
   Node n1 = Node();
-
-  n1.setId(45);
-
-  CHECK(n1.getId() == 45);
+  n1.setId(0);
+  CHECK(n1.getId() == 0);
+  Node n2 = Node();
+  n2.setId(1);
+  CHECK(n2.getId() == 1);
 }
 
 TEST_CASE("Setting label (Node)") {
