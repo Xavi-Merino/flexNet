4741dda392fea29f4fb5b8f2316f5bc6c84691be
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-01-05 18:21:30 -0300

    Changing references of getLink and putting link in network. Checking also the copy and the reference

--- test/test_network.cpp
+++ test/test_network.cpp
@@ -23,7 +23,7 @@ TEST_CASE("Getting Node position (Network)") {
 TEST_CASE("Getting Link position (Network)") {
   Network n1 = Network();
   n1.addLink(Link(0));
-  CHECK(n1.getLink(0)->getId() == 0);
+  CHECK(n1.getLink(0).getId() == 0);
   CHECK_THROWS(n1.getLink(-1));
   CHECK_THROWS(n1.getLink(-50));
 };
@@ -34,7 +34,7 @@ TEST_CASE("Copy constructor (Network)") {
   n1.addLink(Link(0));
   Network n2 = Network(n1);
   CHECK(n1.getNode(0).getId() == n2.getNode(0).getId());
-  CHECK(n1.getLink(0)->getId() == n2.getLink(0)->getId());
+  CHECK(n1.getLink(0).getId() == n2.getLink(0).getId());
   CHECK_THROWS(n1.getNode(-1).getId() == n2.getNode(-1).getId());
 };
 
@@ -48,7 +48,7 @@ TEST_CASE("Connect source and destination nodes (Network)") {
   net1.addNode(Node(1));
   n2_pos = net1.getNode(1).getId();
   net1.addLink(Link(0));
-  l1_pos = net1.getLink(0)->getId();
+  l1_pos = net1.getLink(0).getId();
   net1.connect(n1_pos, l1_pos, n2_pos);
   CHECK(net1.isConnected(n1_pos, n2_pos) == true);
 };
@@ -59,8 +59,10 @@ TEST_CASE("JSON constructor") {
 
 TEST_CASE("Use slot (Network)") {
   Network net1 = Network();
-  Link l1 = Link(20, 70.0, 200);
+  Link l1 = Link(0, 70.0, 200);
+  net1.addLink(l1);
   CHECK(l1.getSlot(100) == false);
   net1.useSlot(0, 100);
-  CHECK(l1.getSlot(100) == true);
+  CHECK(l1.getSlot(100) == false);
+  CHECK(net1.getLink(0).getSlot(100) == true);
 }
