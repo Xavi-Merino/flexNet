082dd4dbbf90a18fe33f14330333c5ae42ab57e3
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-01-11 17:58:51 -0300

    Throwing exception with a negative lambda

--- src/exp_variable.cpp
+++ src/exp_variable.cpp
@@ -1,7 +1,11 @@
 #include "exp_variable.hpp"
 
 ExpVariable::ExpVariable(unsigned int seed, double parameter1)
-    : RandomVariable(seed, parameter1) {}
+    : RandomVariable(seed, parameter1) {
+  if (parameter1 <= 0) {
+    throw std::runtime_error("Lambda parameter must be positive.");
+  }
+}
 
 double ExpVariable::getNextValue() {
   return (-log(1 - dist(this->generator)) / this->parameter1);
--- src/exp_variable.hpp
+++ src/exp_variable.hpp
@@ -1,6 +1,8 @@
 #ifndef __EXP_VARIABLE_H__
 #define __EXP_VARIABLE_H__
 
+#include <stdexcept>
+
 #include "random_variable.hpp"
 
 class ExpVariable : public RandomVariable {
