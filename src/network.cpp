9cbb558f93fdedd326f3a06244729e726d79a332
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-01-15 09:40:08 -0300

    adding exception to connect method

--- src/network.cpp
+++ src/network.cpp
@@ -120,6 +120,24 @@ void Network::addLink(Link link) {
 void Network::connect(int src, int link,
                       int dst)  // Using Ids and Link from Nodes/Links vectors
 {
+  if (src < 0 || src >= this->node_counter) {
+    throw std::runtime_error(
+        "Cannot connect src " + std::to_string(src) +
+        " because its ID is not in the network. Number of nodes in network: " +
+        std::to_string(this->node_counter));
+  }
+  if (dst < 0 || dst >= this->node_counter) {
+    throw std::runtime_error(
+        "Cannot connect dst " + std::to_string(dst) +
+        " because its ID is not in the network. Number of nodes in network: " +
+        std::to_string(this->node_counter));
+  }
+  if (src < 0 || src >= this->node_counter) {
+    throw std::runtime_error(
+        "Cannot use link " + std::to_string(link) +
+        " because its ID is not in the network. Number of links in network: " +
+        std::to_string(this->link_counter));
+  }
   this->links_out.insert(this->links_out.begin() + this->nodes_out.at(src),
                          &this->links.at(link));
   std::for_each(this->nodes_out.begin() + src + 1, this->nodes_out.end(),
