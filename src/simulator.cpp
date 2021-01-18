82386bbd1a94c58d43add52d1ba75b2656933150
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-01-18 12:09:20 -0300

    Constructors and default values of Simulator class

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,12 +1,85 @@
 #include "simulator.hpp"
 
 Simulator::Simulator(void) {
-  this->clock = 0;
-  this->arriveVariable = ExpVariable(123456, 3);
-  this->departVariable = ExpVariable(123456, 10);
+  this->defaultValues();
   this->controller = Controller();
+  this->events = std::list<Event>();
+  this->bitRatesDefault =
+      std::vector<double>({10.0, 40.0, 100.0, 400.0, 1000.0});
+}
+
+Simulator::Simulator(std::string networkFilename, std::string pathFilename) {
+  this->defaultValues();
+  this->controller = Controller();
+  this->controller.setNetwork(Network(networkFilename));
+  this->controller.setPaths(pathFilename);
+  this->events = std::list<Event>();
+  this->bitRatesDefault =
+      std::vector<double>({10.0, 40.0, 100.0, 400.0, 1000.0});
 }
 
 void Simulator::run(void) {}
 
-void Simulator::init(void) {}
+void Simulator::setLambda(double lambda) { this->lambda = lambda; }
+
+void Simulator::setMu(double mu) { this->mu = mu; }
+
+void Simulator::setSeedArrive(unsigned int seed) { this->seedArrive = seed; }
+
+void Simulator::setSeedDeparture(double seed) { this->seedDeparture = seed; }
+
+void Simulator::setGoalConnections(long long goal) {
+  this->goalConnections = goal;
+}
+
+void Simulator::setBitRates(std::vector<double> bitRates) {
+  this->bitRatesDefault = std::vector<double>(bitRates);
+}
+
+void Simulator::defaultValues() {
+  this->lambda = 3;
+  this->mu = 10;
+  this->seedArrive = 12345;
+  this->seedDeparture = 12345;
+  this->seedSrc = 12345;
+  this->seedDst = 12345;
+  this->numberOfConnections = 0;
+  this->goalConnections = 10000;
+}
+
+int Simulator::eventRoutine(void) {
+  this->currentEvent = this->events.front();
+  this->rtnAllocation = -1;
+  if (this->currentEvent.getType() == ARRIVE) {
+    nextEventTime = this->clock + this->arriveVariable.getNextValue();
+    for (std::list<Event>::reverse_iterator pos = this->events.rbegin();
+         pos != this->events.rend(); pos++) {
+      if (pos->getTime() < nextEventTime) {
+        this->events.insert(++pos.base(), Event(ARRIVE, nextEventTime,
+                                                this->numberOfConnections++));
+      }
+    }
+    src = this->srcVariable.getNextIntValue();
+    dst = this->dstVariable.getNextIntValue();
+    while (src == dst) {
+      dst = this->dstVariable.getNextIntValue();
+    }
+    this->rtnAllocation = this->controller.assignConnection(src, dst, 10);
+  } else if (this->currentEvent.getType() == DEPARTURE) {
+  }
+  this->events.pop_front();
+  return this->rtnAllocation;
+}
+
+void Simulator::init(void) {
+  this->clock = 0;
+  this->arriveVariable = ExpVariable(this->seedArrive, this->lambda);
+  this->departVariable = ExpVariable(this->seedDeparture, this->mu);
+  this->srcVariable = UniformVariable(
+      this->seedSrc, this->controller.getNetwork().getNumberOfNodes() - 1);
+  this->dstVariable = UniformVariable(
+      this->seedDst, this->controller.getNetwork().getNumberOfNodes() - 1);
+  this->events.push_back(Event(ARRIVE, this->arriveVariable.getNextValue(),
+                               this->numberOfConnections++));
+  this->bitRates = this->bitRatesDefault;
+}
--- src/simulator.hpp
+++ src/simulator.hpp
@@ -6,19 +6,48 @@
 #include "controller.hpp"
 #include "event.hpp"
 #include "exp_variable.hpp"
+#include "uniform_variable.hpp"
 
 class Simulator {
  public:
   Simulator(void);
+  Simulator(std::string networkFilename, std::string pathFilename);
   void init(void);
   void run(void);
+
+  void setLambda(double lambda);
+  void setMu(double mu);
+  void setSeedArrive(unsigned int seed);
+  void setSeedDeparture(double seed);
+  void setGoalConnections(long long goal);
+  void setBitRates(std::vector<double> bitRates);
+  void defaultValues();
 
  private:
   double clock;
   std::list<Event> events;
   ExpVariable arriveVariable;
   ExpVariable departVariable;
+  UniformVariable srcVariable;
+  UniformVariable dstVariable;
   Controller controller;
+  Event currentEvent;
+  std::vector<double> bitRates;
+
+  double lambda;
+  double mu;
+  unsigned int seedArrive;
+  unsigned int seedDeparture;
+  unsigned int seedSrc;
+  unsigned int seedDst;
+  long long numberOfConnections;
+  long long goalConnections;
+  double nextEventTime;
+  int rtnAllocation;
+  int src, dst;
+  std::vector<double> bitRatesDefault;
+
+  int eventRoutine(void);
 };
 
 #endif
