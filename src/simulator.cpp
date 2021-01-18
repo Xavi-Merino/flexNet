82c127f5aa786583c29c761dc40271833804d967
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-01-18 10:22:05 -0300

    adding firts default simulator constructor

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,6 +1,11 @@
 #include "simulator.hpp"
 
-Simulator::Simulator(void) {}
+Simulator::Simulator(void) {
+  this->clock = 0;
+  this->arriveVariable = ExpVariable(123456, 3);
+  this->departVariable = ExpVariable(123456, 10);
+  this->controller = Controller();
+}
 
 void Simulator::run(void) {}
 
