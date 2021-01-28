d49c4311889d104c1d4a2d57c61dc839214e3466
Gonzalo España <gespana@alumnos.uai.cl>
2021-01-28 16:12:31 -0300

    comments of event.hpp

--- src/event.hpp
+++ src/event.hpp
@@ -4,11 +4,28 @@
 // #include "simulator.hpp"
 #include "controller.hpp"
 
+/**
+ * @brief   Class event
+ *
+ * This class contains the event that happens in the network of nodes
+ */
+
 typedef enum eventType { ARRIVE, DEPARTURE } eventType;
 class Event {
  public:
+  /**
+   * @brief    Constructor of empyy event
+   */
   Event(void);
+  /**
+   * @brief    Constructor of the event with the data types: eventType, double,
+   * long long
+   */
   Event(eventType type, double time, long long idConnection);
+
+  /**
+   * @brief    Delete the Event object
+   */
 
   ~Event();
 
