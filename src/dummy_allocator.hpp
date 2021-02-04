ad9e51754ab8287df78b13f8345799fdae89d01f
Gonzalo España <gespana@alumnos.uai.cl>
2021-02-03 22:44:20 -0300

    documentation dummy_allocator.hpp

--- src/dummy_allocator.hpp
+++ src/dummy_allocator.hpp
@@ -3,12 +3,36 @@
 
 #include "allocator.hpp"
 
+/**
+ * @brief Class "DummyAllocator" which extends class father "Allocator".
+ */
+
 class DummyAllocator : public Allocator {
  public:
+  /**
+   * @brief Constructs a new DummyAllocator object.
+   */
   DummyAllocator();
+  /**
+   * @brief Constructs a new DummyAllocator object and initialize the network.
+   *
+   * @param network pointer type network representing the network of nodes.
+   */
   DummyAllocator(Network *network);
+  /**
+   * @brief Delete the object DummyAllocator.
+   */
   ~DummyAllocator();
-
+  /**
+   * @brief
+   *
+   * @param src
+   * @param dst
+   * @param bitRate
+   * @param con
+   *
+   * @return
+   */
   allocationStatus exec(int src, int dst, BitRate bitRate, Connection &con);
 };
 
