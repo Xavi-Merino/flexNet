7db891ec0d1313db7c6104a17d016e94033bb341
Diana Marino <dmarino@alumnos.uai.cl>
2021-02-04 10:01:08 -0300

    Adding changes to link documentation

--- src/link.hpp
+++ src/link.hpp
@@ -11,93 +11,103 @@
 /**
  * @brief Class with the link information.
  *
- * This class represents to each link in the physical network.
- *
+ * This class is used to create and access link objects, which represent the
+ * links between the nodes in the network. Each link is divided in slots.
  */
 
 class Link {
  public:
   /**
-   * @brief Construct a new Link object. When calling with no parameters the
+   * @brief Constructs a new Link object. When calling with no parameters the
    * default link id is -1, the default link length is 100.0 and the default
    * slots are 320.
    *
    */
   Link(void);
   /**
-   * @brief Construct a new Link object
+   * @brief Constructs a new Link object. The parameter is assigned to the id
+   * attribute of the node, while the link length and slots are set to 100.0 and
+   * 320 respectively.
    *
-   * @param id The id of this new Link object
+   * @param id The id of this new Link object.
    */
   Link(int id);
   /**
-   * @brief Construct a new Link object
+   * @brief Constructs a new Link object. The parameters are assigned to the id
+   * and length attributes of the node, while the slots are set to 320.
    *
-   * @param id The id of this new Link object
-   * @param length The length of this new Link object
+   * @param id The id of this new Link object.
+   * @param length The length of this new Link object.
    */
   Link(int id, float length);
   /**
-   * @brief Construct a new Link object
+   * @brief Construct a new Link object. The parameters are assigned to the id,
+   * length and slots attributes.
    *
-   * @param id The id of this new Link object
-   * @param length The length of this new Link object
-   * @param slots The size of the Slots vector of this new Link object
+   * @param id The id of this new Link object.
+   * @param length The length of this new Link object.
+   * @param slots The size of the Slots vector of this new Link object.
    */
   Link(int id, float length, int slots);
   /**
-   * @brief Destroy the Link object
+   * @brief Destroys the Link object.
    *
    */
   ~Link();
   /**
-   * @brief Set the Id object
+   * @brief Set the Id of a link. This method only works on links that were
+   * created without a specified id.
    *
-   * @param id The new Id of this Link object
+   * @param id The new Id of this Link object.
    */
   void setId(int id);
   /**
-   * @brief Set the Length object
+   * @brief Set the Length of the link. The value must be greater than or equal
+   * to zero.
    *
-   * @param length The new Length of this Link object
+   * @param length The new Length of this Link object.
    */
   void setLength(float length);
   /**
-   * @brief Set the size of the Slots object
+   * @brief Change the size of the Slots vector of the link. The value must
+   * be greater than or equal to 1 and every element on the previous vector must
+   * be set to false, which represents an inactive slot.
    *
-   * @param slots The new size of the Slots vector of this Link object
+   * @param slots The new size of the Slots vector of this Link object.
    */
   void setSlots(int slots);
   /**
-   * @brief Set the value of a specific Slot
+   * @brief Set the value of a specific Slot inside the slots vector. The
+   * position indicated must be inside the bounds of the vector, and the value
+   * parameter is assigned to the corresponding slot.
    *
-   * @param pos The position of the Slot inside the Slots vector
-   * @param value The value desired to assign to the specified Slot
+   * @param pos The position of the Slot inside the Slots vector.
+   * @param value The state (active or inactive) to be assigned to the specified
+   * Slot.
    */
   void setSlot(int pos, bool value);
   /**
-   * @brief Get the Id object
+   * @brief Get the Id of the link.
    *
-   * @return int The Id of this Link object
+   * @return int The Id of this Link object.
    */
   int getId(void) const;
   /**
-   * @brief Get the Length object
-   *
-   * @return float The Length of this Link object
+   * @brief Get the Length attribute of the link.
+   * @return float The Length of this Link object.
    */
   float getLength(void) const;
   /**
-   * @brief Get the size of the Slots object
+   * @brief Get the size of the Slots vector of the link.
    *
-   * @return int The Id of this Link object
+   * @return int The size of the slots vector.
    */
   int getSlots(void) const;
   /**
-   * @brief Get the value of a specific Slot
+   * @brief Get the state of a specific Slot inside the slots vector.
    *
-   * @param pos The position of the Slot in the Slots vector
-   * @return bool The value of the specified Slot
+   * @param pos The position of the Slot in the Slots vector.
+   * @return bool The state of the specified Slot.
    */
   bool getSlot(int pos) const;
 
