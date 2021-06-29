d1fe53261c40a599c85bd3b7e8bf49fd7e0fb877
Felipe <felipefalconurrutia@gmail.com>
2021-06-29 09:29:23 -0400

    Test(Allocator): Added Allocator's test cases inside Constructor's tests

--- test/test_controller.cpp
+++ test/test_controller.cpp
@@ -5,3 +5,30 @@
 #include "catch.hpp"
 
 TEST_CASE("Constructor (Controller)") { CHECK_NOTHROW(Controller()); }
+
+// Allocator Testing
+TEST_CASE("Constructor (Allocator)") { CHECK_NOTHROW(Allocator()); }
+
+TEST_CASE("Constructor (Network) with JSON file") {
+  Network *networkExample;
+  CHECK_NOTHROW(Allocator(networkExample));
+};
+
+TEST_CASE("Destructor (Allocator)") {
+  Allocator *asignador = new Allocator;
+  CHECK_NOTHROW(delete asignador);
+}
+
+TEST_CASE("AllocationStatus (Allocator) through Controller") {
+  Controller controllerExample;
+  Allocator allocatorExample;
+  controllerExample.setAllocator(&allocatorExample);
+  int src = 0;
+  int dst = 1;
+  long long idConnection = 0;
+  // Connection con = Connection(idConnection);
+  BitRate bitRate(100.0);
+  CHECK_THROWS(
+      controllerExample.assignConnection(src, dst, bitRate, idConnection) ==
+      (ALLOCATED || NOT_ALLOCATED));
+}
