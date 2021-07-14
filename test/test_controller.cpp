17873d7d99650a84e0a9ed16462f13c7afbe42dd
gonzaloespana <gespana@alumnos.uai.cl>
2021-07-13 20:44:52 -0400

    test controller constructor

--- test/test_controller.cpp
+++ test/test_controller.cpp
@@ -4,4 +4,10 @@
 #include "../src/controller.hpp"
 #include "catch.hpp"
 
-TEST_CASE("Constructor (Controller)") { CHECK_NOTHROW(Controller()); }
+TEST_CASE("Constructor (Controller)")
+{
+    CHECK_NOTHROW(Controller());
+
+    Network net = Network();
+    CHECK_NOTHROW(Controller(net))
+}
