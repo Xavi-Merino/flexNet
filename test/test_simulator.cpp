5a29edb774f53072ca3dada28802d53ccdc2ecc7
gonzaloespana <gespana@alumnos.uai.cl>
2021-07-17 22:52:52 -0400

    test cases simulator

--- test/test_simulator.cpp
+++ test/test_simulator.cpp
@@ -2,7 +2,8 @@
 
 #include "../src/simulator.hpp"
 #include "catch.hpp"
-BEGIN_ALLOC_FUNCTION(macros) {
+BEGIN_ALLOC_FUNCTION(macros)
+{
   REQ_SLOTS(0);
   REQ_REACH(0);
   REQ_MODULATION(0);
@@ -12,28 +13,38 @@ BEGIN_ALLOC_FUNCTION(macros) {
 }
 END_ALLOC_FUNCTION
 
-BEGIN_ALLOC_FUNCTION(ExactFit) {
+BEGIN_ALLOC_FUNCTION(ExactFit)
+{
   int numberOfSlots = REQ_SLOTS(0);
   int currentNumberSlots;
   int currentSlotIndex;
   int firstIndex;
   std::vector<bool> totalSlots =
       std::vector<bool>(LINK_IN_ROUTE(0, 0)->getSlots(), false);
-  for (int i = 0; i < NUMBER_OF_ROUTES; i++) {
+  for (int i = 0; i < NUMBER_OF_ROUTES; i++)
+  {
     firstIndex = -1;
-    for (int j = 0; j < NUMBER_OF_LINKS(i); j++) {
-      for (int k = 0; k < LINK_IN_ROUTE(i, j)->getSlots(); k++) {
+    for (int j = 0; j < NUMBER_OF_LINKS(i); j++)
+    {
+      for (int k = 0; k < LINK_IN_ROUTE(i, j)->getSlots(); k++)
+      {
         totalSlots[k] = totalSlots[k] | LINK_IN_ROUTE(i, j)->getSlot(k);
       }
     }
     currentNumberSlots = 0;
     currentSlotIndex = 0;
-    for (int j = 0; j < totalSlots.size(); j++) {
-      if (totalSlots[j] == false) {
+    for (int j = 0; j < totalSlots.size(); j++)
+    {
+      if (totalSlots[j] == false)
+      {
         currentNumberSlots++;
-      } else {
-        if (currentNumberSlots == numberOfSlots) {
-          for (int j = 0; j < NUMBER_OF_LINKS(i); j++) {
+      }
+      else
+      {
+        if (currentNumberSlots == numberOfSlots)
+        {
+          for (int j = 0; j < NUMBER_OF_LINKS(i); j++)
+          {
             ALLOC_SLOTS(LINK_IN_ROUTE_ID(i, j), currentSlotIndex, numberOfSlots)
           }
           return ALLOCATED;
@@ -41,12 +52,15 @@ BEGIN_ALLOC_FUNCTION(ExactFit) {
         currentNumberSlots = 0;
         currentSlotIndex = j + 1;
       }
-      if (firstIndex == -1 && currentNumberSlots > numberOfSlots) {
+      if (firstIndex == -1 && currentNumberSlots > numberOfSlots)
+      {
         firstIndex = currentSlotIndex;
       }
     }
-    if (firstIndex != -1) {
-      for (int j = 0; j < NUMBER_OF_LINKS(i); j++) {
+    if (firstIndex != -1)
+    {
+      for (int j = 0; j < NUMBER_OF_LINKS(i); j++)
+      {
         ALLOC_SLOTS(LINK_IN_ROUTE_ID(i, j), firstIndex, numberOfSlots)
       }
       return ALLOCATED;
@@ -56,7 +70,8 @@ BEGIN_ALLOC_FUNCTION(ExactFit) {
 }
 END_ALLOC_FUNCTION
 
-TEST_CASE("MACROS with FF andd 100 simulations (Simulator)") {
+TEST_CASE("MACROS with FF andd 100 simulations (Simulator)")
+{
   Simulator s = Simulator(std::string("../networks/NSFNet.json"),
                           std::string("../networks/NSFNet_routes.json"),
                           std::string("../bitrate/bitrate.json"));
@@ -65,7 +80,8 @@ TEST_CASE("MACROS with FF andd 100 simulations (Simulator)") {
   CHECK_NOTHROW(s.run());
 }
 
-TEST_CASE("Algorithm - metrics (Simulator)") {
+TEST_CASE("Algorithm - metrics (Simulator)")
+{
   Simulator s = Simulator(std::string("../networks/NSFNet.json"),
                           std::string("../networks/NSFNet_routes.json"),
                           std::string("../bitrate/bitrate.json"));
@@ -76,3 +92,47 @@ TEST_CASE("Algorithm - metrics (Simulator)") {
   CHECK_NOTHROW(s.getBlockingProbability());
   CHECK_NOTHROW(s.getTimeDuration());
 }
+
+TEST_CASE("Constructor - (Simulator)")
+{
+  CHECK_NOTHROW(Simulator());
+  CHECK_NOTHROW(Simulator("../networks/NSFNet.json", "../networks/NSFNet_routes.json"));
+  CHECK_NOTHROW(Simulator("../networks/NSFNet.json", "../networks/NSFNet_routes.json", "../bitrate/bitrate.json"));
+}
+
+TEST_CASE("Set lambda")
+{
+  Simulator s = Simulator();
+  double lambda = 8.16;
+  CHECK_NOTHROW(s.setLambda(lambda));
+}
+
+TEST_CASE("Set mu")
+{
+  Simulator s = Simulator();
+  double mu = 4.3;
+  CHECK_NOTHROW(s.setMu(mu));
+}
+
+TEST_CASE("Set seed arrive")
+{
+  Simulator s = Simulator();
+  unsigned int seed = 32;
+  CHECK_NOTHROW(s.setSeedArrive(seed));
+}
+
+TEST_CASE("Set seed departure")
+{
+  Simulator s = Simulator();
+  unsigned int seed = 32;
+  CHECK_NOTHROW(s.setSeedDeparture(seed));
+}
+
+TEST_CASE("Set seed bit rate")
+{
+  Simulator s = Simulator();
+  unsigned int seed = 32;
+  CHECK_NOTHROW(s.setSeedDeparture(seed));
+}
+
+// set goal connection test case to do
