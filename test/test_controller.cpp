5adef57aa8b4b2a355adc86c97247a90172071b2
gonzaloespana <gespana@alumnos.uai.cl>
2021-07-19 19:36:32 -0400

    Format changes

--- test/test_controller.cpp
+++ test/test_controller.cpp
@@ -4,23 +4,22 @@
 #include "../src/controller.hpp"
 #include "catch.hpp"
 
-TEST_CASE("Constructor (Controller)")
-{
-    CHECK_NOTHROW(Controller());
-    int n1_pos;
-    int n2_pos;
-    int l1_pos;
-    Network *net = Network();
+TEST_CASE("Constructor (Controller)") {
+  CHECK_NOTHROW(Controller());
+  int n1_pos;
+  int n2_pos;
+  int l1_pos;
+  Network *net = Network();
 
-    net.addNode(new Node(0));
-    n1_pos = net.getNode(0)->getId();
+  net.addNode(new Node(0));
+  n1_pos = net.getNode(0)->getId();
 
-    net.addNode(new Node(1));
-    n2_pos = net.getNode(1)->getId();
-    net.addLink(new Link(0));
+  net.addNode(new Node(1));
+  n2_pos = net.getNode(1)->getId();
+  net.addLink(new Link(0));
 
-    l1_pos = net.getLink(0)->getId();
-    net.connect(n1_pos, l1_pos, n2_pos);
+  l1_pos = net.getLink(0)->getId();
+  net.connect(n1_pos, l1_pos, n2_pos);
 
-    CHECK_NOTHROW(Controller(net));
+  CHECK_NOTHROW(Controller(net));
 }
