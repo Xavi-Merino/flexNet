b36ca39de8f8d688501dd58824f314a4553363de
gonzaloespana <gespana@alumnos.uai.cl>
2021-07-21 17:13:36 -0400

    Test format JSON file paths

--- test/test_controller.cpp
+++ test/test_controller.cpp
@@ -1,7 +1,10 @@
 // Let Catch provide main():
 #define CATCH_CONFIG_MAIN
 
+#include <fstream>
+
 #include "../src/controller.hpp"
+#include "../src/json.hpp"
 #include "catch.hpp"
 
 TEST_CASE("Constructor (Controller)") {
@@ -22,4 +25,15 @@ TEST_CASE("Constructor (Controller)") {
   net.connect(n1_pos, l1_pos, n2_pos);
 
   CHECK_NOTHROW(Controller(net));
+}
+
+TEST_CASE("Correct format JSON file paths (Controller)") {
+  std::ifstream file("../networks/NSFNet_routes.json");
+  nlohmann::json jsonRoutes;
+  file >> jsonRoutes;
+
+  CHECK_NOTHROW(jsonRoutes["routes"]);
+  CHECK_NOTHROW(jsonRoutes["routes"][0]["paths"]);
+  CHECK_NOTHROW(jsonRoutes["routes"][0]["src"]);
+  CHECK_NOTHROW(jsonRoutes["routes"][0]["dst"]);
 }
